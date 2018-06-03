0f90d051917fd556618e863e2e6ed857ef0f141b
Holly Ho <hollyhaho@gmail.com>
2018-06-02 19:36:04 -0700

    Added client html along with adding image property to stories object

--- getDataFromVietBao.js
+++ getDataFromVietBao.js
@@ -20,6 +20,7 @@ function getChildrenStories(){
     .then(function(htmlString0){
         
         parseHTML(htmlString0)
+    
         return(childrenStoriesRAW)
         // return(rp(urls[1]))
     })
@@ -61,14 +62,28 @@ function parseHTML(htmlString) {
     var pl_list_col_1_that_holds_stories = $("div.pl_list.pl_list_col_1").eq(indexOfStories).html();
     $ = cheerio.load(pl_list_col_1_that_holds_stories);
     var $titleOfStories = $("div.pl_row div.pl_title a");
+
+    // linke to get pictures https://vietbao.com/images/file/bzAorXrD1QgBAOlp/w150/dothiphuong.jpg
     for (var i = 0; i < $titleOfStories.length; i++) {
         var title = $("div.pl_row div.pl_title a")[i].attribs.title;
         var link = "https://vietbao.com/" + $("div.pl_row div.pl_title a")[i].attribs.href;
         var date = $("div.pl_row span.pl_date").eq(i).html();
+        var image
+
+        if ($("div.pl_row div.pl_thumbnail img")[i] === undefined ) {
+            image = null 
+        } else {
+            
+            image = "https://vietbao.com/" + $("div.pl_row div.pl_thumbnail img")[i].attribs['data-original']
+
+            
+        }
+
         var newStory = {
             title: title,
             link: link,
-            date: date
+            date: date,
+            image: image
         };
         childrenStoriesRAW.push(newStory);
     }
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require("express");
+const express = require("express")
 const service = require("./getDataFromVietBao")
+const bodyParser = require("body-parser")
 
 const app = express();
-const port = (process.env.PORT || 3000);
+const port = (process.env.PORT || 5000);
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + "/Client"));
 
 app.get("/stories", function(req, res){
    service.getChildrenStories()
     .then(function(data){
-        console.log(data)
+        // console.log(data)
         // console.log(data.length)
         res.send(data)
     })
