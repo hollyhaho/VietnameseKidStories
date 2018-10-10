4d2acfca522af93bb7bad7d38f8c32ec569db18c
Holly Ho <hollyhaho.gmail.com>
2018-10-10 12:10:50 -0700

    some changes to get me to work on this project again.

--- getDataFromVietBao.js
+++ getDataFromVietBao.js
@@ -1,12 +1,16 @@
 const rp = require('request-promise');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+const Promise = require('bluebird');
 
-var urls = [
-    "https://vietbao.com/p131/1/thieu-nhi",
-    "https://vietbao.com/p131/2/thieu-nhi",
-    "https://vietbao.com/p131/3/thieu-nhi",
-    "https://vietbao.com/p131/4/thieu-nhi",
-]
+var urls = generateUrls();
+function generateUrls(){
+    var urls = []
+    for (var i = 1; i < 5; i++) {
+        var link = 'https://vietbao.com/p131/' + i + "/thieu-nhi"
+        urls.push(link)
+    }
+    return urls
+}
 var childrenStoriesRAW = []
 var childrenStoriesFormated = {}
 
@@ -14,16 +18,35 @@ var thuThieuNhiStories = []
 var tamTinhThayTro = []
 var beVietVanViet = []
 
+function groupBy(list, keyGetter){
+    const map = new Map();
+    list.forEach((item) => {
+        const key = keyGetter(item)
+        const collection = map.get(key)
+        if (!collection){
+            map.set(key, [item])
+        } else {
+            collection.push(item)
+        }
+    });
+    return map
+}
+
+
 function getChildrenStories(){
-    
-    return rp(urls[0])
+    generateUrls()
+    console.log(urls[2]);
+    return rp(urls[2])
     .then(function(htmlString0){
         
         parseHTML(htmlString0)
-    
-        return(childrenStoriesRAW)
+        const grouped = groupBy(childrenStoriesRAW, childrenStory => childrenStory.date)
+        // console.log(grouped)
+        // return(childrenStoriesRAW)
         // return(rp(urls[1]))
     })
+
+   
     // .then(function(htmlString1){
     //     parseHTML(htmlString1)
     //     return(rp(urls[2]))
@@ -51,19 +74,39 @@ function getChildrenStories(){
    
 }
 
+function getChildrenStories2(){
+    
+    // credit to 
+    // https://stackoverflow.com/questions/39649237/node-js-web-scraping-with-loop-on-array-of-urls
+    return Promise.map(urls, rp)
+    .map((htmlOnPage, index)=> {
+        parseHTML(htmlOnPage)
+    })
+    .then(function(){
+        // After all the mapping is done
+        // return childnStoriesRaw which now has all stories
+        // from all the urls in the urls array
+        return(childrenStoriesRAW)
+    })
+    .catch((e) => console.log("We encountered an error" + e));
+    
+}
+
 module.exports = {
-    getChildrenStories
+    getChildrenStories,
+    getChildrenStories2
 }
 
 function parseHTML(htmlString) {
     var indexOfStories = 1;
     var $ = cheerio.load(htmlString);
-    // Gets 2nd item array that holds all of pl_list pl_list_col_1 dom objects
+    // Gets 2nd item in the array that holds all of pl_list pl_list_col_1 dom objects
     var pl_list_col_1_that_holds_stories = $("div.pl_list.pl_list_col_1").eq(indexOfStories).html();
     $ = cheerio.load(pl_list_col_1_that_holds_stories);
     var $titleOfStories = $("div.pl_row div.pl_title a");
 
-    // linke to get pictures https://vietbao.com/images/file/bzAorXrD1QgBAOlp/w150/dothiphuong.jpg
+    // generate link to get pictures
+    // ex url: https://vietbao.com/images/file/bzAorXrD1QgBAOlp/w150/dothiphuong.jpg
     for (var i = 0; i < $titleOfStories.length; i++) {
         var title = $("div.pl_row div.pl_title a")[i].attribs.title;
         var link = "https://vietbao.com/" + $("div.pl_row div.pl_title a")[i].attribs.href;
@@ -73,10 +116,7 @@ function parseHTML(htmlString) {
         if ($("div.pl_row div.pl_thumbnail img")[i] === undefined ) {
             image = null 
         } else {
-            
             image = "https://vietbao.com/" + $("div.pl_row div.pl_thumbnail img")[i].attribs['data-original']
-
-            
         }
 
         var newStory = {
--- server.js
+++ server.js
@@ -10,15 +10,36 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/Client"));
 
 app.get("/stories", function(req, res){
-   service.getChildrenStories()
+   service.getChildrenStories2()
     .then(function(data){
         // console.log(data)
         console.log(data.length)
-        console.log(data)
+        // console.log(data)
+
         res.send(data)
     })
+    // stories = service.getChildrenStories2()
+    
+    
+
 })
 
+app.get("/stories/BeVietVanViet", function(req, res){
+    var beVietVanViet = []
+    
+    service.getChildrenStories2()
+    .then(function(data){
+        for (var i = 0; i < data.length; i++){
+            if (/Bé Viết Văn Việt/.test(data[i].title)){
+                beVietVanViet.push(data[i])
+            }
+    
+        }
+
+        res.send(beVietVanViet)
+    })
+
+})
 app.listen(port, function(){
     console.log("App listening on port " + port);
 })
